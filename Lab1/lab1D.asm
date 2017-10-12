f8dbbb6ae2b22df1622edccdb7e88026be7d377b
ADozois <dozois.a@gmail.com>
2017-10-12 11:43:09 -0400

    Final Commit

--- Lab1/lab1D.asm
+++ Lab1/lab1D.asm
@@ -20,6 +20,14 @@
 
 ROMStart    EQU  $4000  ; absolute address to place my code/constant data
 
+Avance:	    EQU		3200		; Vitesse maximale d'avancement
+Neutre: 	EQU		3000		; Valeurs d'arr?t
+Recule: 	EQU		2800		; Vitesse maximale de recule
+
+Rapide:     EQU     $0C80
+Moyenne:    EQU     $0C1C
+Lente:      EQU     $0BEA
+
 ; variable/data section
 
             ORG RAMStart
@@ -35,30 +43,57 @@ VMG:	DS.W	30			; Vitesse moteur gauche
 COMPT	DS.B	1			; Compteur
 INDEX	DS.B	1			; index du tableau
 
-Avance:	EQU		3200		; Vitesse maximale d'avancement
-Neutre:	EQU		3000		; Valeurs d'arr�t
-Recule:	EQU		2800		; Vitesse maximale de recule
-
 
 ; code section
             ORG   ROMStart
+
+
 Entry:
             CLI                   	; enable interrupts
+            LDS #$1000 ; initialisation de la pileau haut
+                       ; de ls RAM ($0800-$0FFF)
+                       
+            ;*************************************************************************
+            ;
+            ;Init portb en sortie
 
-          	LDD		TTotal
-			LDY		VCst
+             LDAB #$ff ; 1 = sortie
+             STAB DDRB
+             
+            ;*************************************************************************
+            ;
+            ; Init du SCI (transmetteur et récepteur de caractères sériel)
+             CLR SCIBDH
+             LDAB #$34 ; si bus clk est à 8MHz
+             STAB SCIBDL ; 9600 BAUDS
+             CLR SCICR1 ; M BIT = 0 POUR 8 BITS
+             LDAB #$0C
+             STAB SCICR2 ; TE , RE
+
+            ;*************************************************************************
+            
+            ; Demande a l'utilisateur de la vitesse et du temps
+            JSR     GetVitesse
+            JSR     GetTemps
+            
+            ; Phase de calcul
+            LDD     TTotal
+            LDY     VCst
 			JSR		Calcul
 
+            ; Construction du profil
 			LDY		#VMD
 			LDX		#DeltaV
 			JSR		Profil
 
 			; affichage du profil de vitesse
-			LDX			VMD
-        	LDY			VMG
-            JSR			PrintProfil	; essai de la fonction
+			LDX		#VMD
+        	LDY		#VMG
+            JSR		PrintProfil	; essai de la fonction
+            
+            
 
-			BRA		Entry
+			BRN		Entry
 
 ;	Functions
 Calcul:		; Calculer DeltaV, TA, TC
@@ -87,7 +122,7 @@ Calcul:		; Calculer DeltaV, TA, TC
 
 
 ;	Functions
-Profil:		; �crire les valeurs du profil de vitesse dans les tableaux
+Profil:		; ?crire les valeurs du profil de vitesse dans les tableaux
 		;	ramp down
 		MOVB	#10,COMPT 	; Assignation de 10 dans le compteur
 		LDD		#Neutre		; Charger la valeur neutre dans le registre D
@@ -139,26 +174,104 @@ PrintProfil:	; fonction
 		MOVB	#00, INDEX
 
 nextPts:
-		;printf	str1		; "Vitesse "
-		;out2hex	INDEX	; Afficher l'index du tableau
-		;printf	str2		; " : "
-		;out4hex	1,X+	; Afficher la vitesse droite pour l'index actuel
-		;printf	str2		; " : "
-		;out4hex	1,Y+	; Afficher la vitesse gauche pour l'index actuel
-		;printf	CRLF		; CRLF
+		printf	#str1		; "Vitesse "
+		out2hex	INDEX	    ; Afficher l'index du tableau
+		printf	#str2		; " : "
+		out4hex	[?2,X+?]    ; Afficher la vitesse droite pour l'index actuel
+		printf	#str2		; " : "
+		out4hex	[?2,Y+?]	; Afficher la vitesse gauche pour l'index actuel
+		printf	#CRLF		; CRLF
 
 		INC		INDEX		; Incrementer l'index du tableau
 		DEC		COMPT		; Decrementer le compteur
-		BHS		nextPts		; tant que COMPT n'est pas egal a la valeur dans
-
+		BNE		nextPts		; tant que COMPT n'est pas egal a la valeur dans
+        
+        printf	#CRLF		; CRLF
+        
+        printf  #str_TA     ; "TA : "
+        out4hex TA
+        printf	#CRLF		; CRLF
+        
+        printf  #str_TC     ; "TC : "
+        out4hex TC
+        printf	#CRLF		; CRLF
+        
+        printf  #str_TT     ; "Ttoatal : "
+        out4hex TTotal
+        printf	#CRLF		; CRLF
+        
+        printf  #str_DV     ; "DeltaV"
+        out4hex DeltaV
+        printf	#CRLF		; CRLF
+        
 		rts
+
+
+; Routine qui demande a l'utilisateur de choisir une vitesse		
+GetVitesse:
+        printf  #QVit
+        getchar
+        CMPB    #$32
+        BLO     ChoixV1
+        BRA     NextV         
+ChoixV1:LDX     #Rapide
+        CMPB    #$31
+        BLO     GetVitesse
+        STX     VCst
+        BRA     VFinis  
+NextV:  CMPB    #$32
+        BHI     ChoixV3
+        BRA     ChoixV2
+ChoixV3:LDD     #Lente
+        CMPB    #$33
+        BHI     GetVitesse
+        STD     VCst
+        BRA     VFinis
+ChoixV2:LDD     #Moyenne
+        STD     VCst
+VFinis: rts   
+
+; Routine qui demande a l'utilisateur de choisir un temps 
+GetTemps:
+        printf  #QTemps
+        getchar
+        CMPB    #$32
+        BLO     ChoixT1
+        BRA     NextT         
+ChoixT1:LDX     #Rapide
+        CMPB    #$31
+        BLO     GetTemps
+        STX     TTotal
+        BRA     TFinis  
+NextT:  CMPB    #$32
+        BHI     ChoixT3
+        BRA     ChoixT2
+ChoixT3:LDD     #Lente
+        CMPB    #$33
+        BHI     GetTemps
+        STD     TTotal
+        BRA     TFinis
+ChoixT2:LDD     #Moyenne
+        STD     TTotal
+TFinis: rts 
+    
 
 ;**************************************************************
 ;*                 Messages textes                            *
 ;**************************************************************
-str1:	dc.b	'Vitesse '
-str2:	dc.b	' : '
-CRLF:	dc.b	$0A,$0D,$00
+str1:	DC.B	'Vitesse ',$00
+str2:	DC.B	' : ',$00
+str_TA: DC.B    'TA : ',$00
+str_TC: DC.B    'TC : ',$00
+str_TT: DC.B    'Ttotal : ',$00
+str_DV: DC.B    'DeltaV : ',$00
+CRLF:	DC.B	$0A,$00
+QVit:   DC.B    'Choisissez une vitesse constante :',$0D
+        DC.B    'Rapide(1), Moyenne(2), Lente(3)',$0A, $0D, $00
+QTemps: DC.B	'Choisissez un temps total :', $0D
+        DC.B    'Rapide(1), Moyenne(2), Lente(3)',$0A, $0D, $00
+        
+
 
 ;*************************************************************************
 ;* 																		 *
