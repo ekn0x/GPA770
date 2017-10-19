c77a8de9e3742be8e2a32a29ad3c61789e2db622
ADozois <dozois.a@gmail.com>
2017-10-18 23:27:29 -0400

    Fuzzy logic implementation

--- Lab1/lab1E.asm
+++ Lab1/lab1E.asm
@@ -19,6 +19,8 @@
             list
 
 ROMStart    EQU  $4000  ; absolute address to place my code/constant data
+MARKER      EQU  $FE            
+ENDRO       EQU  $FF 
 
 ; code section
             ORG   ROMStart
@@ -48,56 +50,128 @@ Entry:
 
             ;*************************************************************************
             
-            ; Demande a l'utilisateur de la vitesse et du temps
-            JSR     GetVitesse
-            JSR     GetTemps
-            
-            ; Phase de calcul
-            LDD     TTotal
-            LDY     VCst
-			JSR		Calcul
+             ; Preparation pour evalution des fonctions membres
+             LDX  #D_LOIN
+             LDY  #D_L
+             LDAA Vcapt_droit 
+             LDAB #$09
+             ; Evaluation des fonction membres
+ENCORE:      MEM
+             DBNE B,ENCORE
+             
+             ; Preparation pour evalution regles
+             LDY  #D_L
+             LDX  #RULESTART
+             LDAA #ENDRO
+             ; Evaluation des regles          
+             REV
+             
+             ; Preparation du calcul de sortie
+             LDX  #SINGLETON
+             LDY  #AGAUCHE
+             LDAB #$05
+             ; Evaluation de la sortie
+             WAV
+             EDIV
+             TFR Y,D
+             
+             STAB COMMANDE
+             
+              
 
-            ; Construction du profil
-			LDY		#VMD
-			LDX		#DeltaV
-			JSR		Profil
-
-			; affichage du profil de vitesse
-			LDX		#VMD
-        	LDY		#VMG
-            JSR		PrintProfil	; essai de la fonction
-            
-            
-
-			BRN		Entry
+FIN:		BRA		FIN
+			
+		
 			
 ;**************************************************************
-;*                          Pentes                            *
+;*                      Variable RAM                          *
+;* NAME:        DS.B                                          *
+;**************************************************************	
+; Voltage capteur
+Vcapt_droit:     DS.W    1
+Vcapt_centre:    DS.W    1
+Vcapt_gauche:    DS.W    1
+
+; Valeur d'appartenance
+D_L:             DS.W    1
+D_M:             DS.W    1
+D_P:             DS.W    1
+C_L:             DS.W    1
+C_M:             DS.W    1
+C_P:             DS.W    1
+G_L:             DS.W    1
+G_M:             DS.W    1
+G_P:             DS.W    1             
+
+; Valeur des regles
+SF_AVANT:        DS.W    1
+SF_AGAUCHE:      DS.W    1
+SF_ADROITE:      DS.W    1
+
+; Valeur de sortie
+AGAUCHE:         DS.W    1
+AVANT:           DS.W    1
+ADROITE:         DS.W    1
+
+; Commande
+COMMANDE:         DS.W    1
+			
+;**************************************************************
+;*                    Function Membre                         *
 ;* NAME:        DC.B    Pts1, Pts2, Pent2, Pente2             *
 ;**************************************************************
 ; Right
-R_VERY_STRONG:  DC.B    $B0, $FF, $10, $00
-R_STRONG:       DC.B    $90, $C0, $10, $10
-R_MEDIUM:       DC.B    $60, $A0, $10, $10
-R_WEAK:         DC.B    $40, $70, $10, $10
-R_VERY_WEAK:    DC.B    $00, $50, $00, $10
+D_LOIN:       DC.B    $00, $30, $00, $10
+D_MIDI:       DC.B    $20, $60, $10, $10
+D_PRES:       DC.B    $50, $FF, $10, $00
 ; Center
-C_VERY_STRONG:  DC.B    $B0, $FF, $10, $00
-C_STRONG:       DC.B    $90, $C0, $10, $10
-C_MEDIUM:       DC.B    $60, $A0, $10, $10
-C_WEAK:         DC.B    $40, $70, $10, $10
-C_VERY_WEAK:    DC.B    $00, $50, $00, $10
+C_LOIN:       DC.B    $00, $30, $00, $10
+C_MIDI:       DC.B    $20, $60, $10, $10
+C_PRES:       DC.B    $50, $FF, $10, $00
 ; Left
-L_VERY_STRONG:  DC.B    $B0, $FF, $10, $00
-L_STRONG:       DC.B    $90, $C0, $10, $10
-L_MEDIUM:       DC.B    $60, $A0, $10, $10
-L_WEAK:         DC.B    $40, $70, $10, $10
-L_VERY_WEAK:    DC.B    $00, $50, $00, $10
+G_LOIN:       DC.B    $00, $30, $00, $10
+G_MIDI:       DC.B    $20, $60, $10, $10
+G_PRES:       DC.B    $50, $FF, $10, $00
+
 
 ;**************************************************************
 ;*                          Rules                             *
-;* NAME:        DC.B    Pts1, Pts2, Pent2, Pente2             *
+;* NAME:        DC.B                                          *
 ;**************************************************************
+RULESTART: DC.W     G_L, C_L, D_L, MARKER, SF_AVANT,   MARKER ;1
+           DC.W     G_L, C_L, D_M, MARKER, SF_AGAUCHE, MARKER ;2
+           DC.W     G_L, C_L, D_P, MARKER, SF_AGAUCHE, MARKER ;3
+           DC.W     G_L, C_M, D_L, MARKER, SF_ADROITE, MARKER ;4
+           DC.W     G_L, C_M, D_M, MARKER, SF_AGAUCHE, MARKER ;5 
+           DC.W     G_L, C_M, D_P, MARKER, SF_AGAUCHE, MARKER ;6 
+           DC.W     G_L, C_P, D_L, MARKER, SF_ADROITE, MARKER ;7 
+           DC.W     G_L, C_P, D_M, MARKER, SF_AGAUCHE, MARKER ;8 
+           DC.W     G_L, C_P, D_P, MARKER, SF_AGAUCHE, MARKER ;9 
+           DC.W     G_M, C_L, D_L, MARKER, SF_ADROITE, MARKER ;10
+           DC.W     G_M, C_L, D_M, MARKER, SF_ADROITE, MARKER ;11 
+           DC.W     G_M, C_L, D_P, MARKER, SF_AGAUCHE, MARKER ;12 
+           DC.W     G_M, C_M, D_L, MARKER, SF_ADROITE, MARKER ;13 
+           DC.W     G_M, C_M, D_M, MARKER, SF_ADROITE, MARKER ;14 
+           DC.W     G_M, C_M, D_P, MARKER, SF_AGAUCHE, MARKER ;15 
+           DC.W     G_M, C_P, D_L, MARKER, SF_ADROITE, MARKER ;16 
+           DC.W     G_M, C_P, D_M, MARKER, SF_ADROITE, MARKER ;17 
+           DC.W     G_M, C_P, D_P, MARKER, SF_AGAUCHE, MARKER ;18 
+           DC.W     G_P, C_L, D_L, MARKER, SF_ADROITE, MARKER ;19 
+           DC.W     G_P, C_L, D_M, MARKER, SF_ADROITE, MARKER ;20 
+           DC.W     G_P, C_L, D_P, MARKER, SF_ADROITE, MARKER ;21 
+           DC.W     G_P, C_M, D_L, MARKER, SF_ADROITE, MARKER ;22 
+           DC.W     G_P, C_M, D_M, MARKER, SF_ADROITE, MARKER ;23 
+           DC.W     G_P, C_M, D_P, MARKER, SF_ADROITE, MARKER ;24 
+           DC.W     G_P, C_P, D_L, MARKER, SF_ADROITE, MARKER ;25 
+           DC.W     G_P, C_P, D_M, MARKER, SF_ADROITE, MARKER ;26 
+           DC.W     G_P, C_P, D_P, MARKER, SF_ADROITE, ENDRO  ;27 
+  
+
+;**************************************************************
+;*                       Singleton                            *
+;* NAME:        DC.B    Liste de valeur                       *
+;**************************************************************
+SINGLETON: DC.B $10, $80, $F0
 
 ;*************************************************************************
 ;* 																		 *
