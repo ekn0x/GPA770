b2aff1c5949ca565313aee56d75c9221026148e6
ADozois <dozois.a@gmail.com>
2017-10-26 10:58:25 -0400

    Working version

--- LAb2/Lab1A.asm
+++ LAb2/Lab1A.asm
@@ -17,9 +17,10 @@
 
 ; include derivative specific macros
             	INCLUDE 	'mc9s12c32.inc'
+            	INCLUDE     'D_BUG12M.mac'
 
 ROMStart    	EQU  		$4000  			; absolute address to place my code/constant data
-Simulateur		EQU			1				; Si le code est compiler pour le simulateur
+Simulateur		EQU			0				; Si le code est compiler pour le simulateur
 
 ; variable/data section
 
@@ -28,36 +29,64 @@ Simulateur		EQU			1				; Si le code est compiler pour le simulateur
 ; code section
             	ORG   		ROMStart
 Entry:
-            	LDS   		#RAMEnd+1       ; initialize the stack pointer
-            	CLI                   		; enable interrupts
-				jsr			initRegistre
+            CLI
+            LDS #$1000
+
+            ;*************************************************************************
+            ;
+            ;Init portb en sortie
+
+             LDAB #$ff ; 1 = sortie
+             STAB DDRB
+             
+            ;*************************************************************************
+            ;
+            ; Init du SCI (transmetteur et récepteur de caractères sériel)
+             CLR SCIBDH
+             LDAB #$34 ; si bus clk est à 8MHz
+             STAB SCIBDL ; 9600 BAUDS
+             CLR SCICR1 ; M BIT = 0 POUR 8 BITS
+             LDAB #$0C
+             STAB SCICR2 ; TE , RE
+			 jsr			initPort
+			 
+				
+				
 mainLoop:
 
-if Simulateur = 0
-				BRSET		PORTP,$01,Action
-else
-				BRCLR		PORTP,$01,Action
-endif
+    IF Simulateur = 0
+                BRCLR		PTP,$01,Action
+    ELSE
+    			BRSET		PTP,$01,Action
+    ENDIF
 	            BRA   		mainLoop        ; restart.
+	            
+DONE:           BRA         DONE
 
 ; Functions
 ; Connection diagram
-;	5V ---- PPSP ---- 470Ohm ---- PERP ---- PORTD --+-- PushButton ---- Gnd
+;	5V ---- PPSP ---- 470Ohm ---- PERP ---- PORTP --+-- PushButton ---- Gnd
 initPort:	; initialisation du registre PTP, pour le polling du push-button
-		BCLR	DDRP,$01		; connect DDRP
-		BSET	PERP,$01		; connect PERP
-		MOVB	#$01,PPSP		; mode pull up
+		BCLR	DDRP,$00		; mode 0 - mode read
+		BSET	PERP,$01		; mode 1 - either pullup or pulldown
+		MOVB	#$00,PPSP		; mode 0 - mode pull up
 		NOP
 		NOP
 		NOP
-		BSET	DDRB,$20
 		rts
 
 Action:
-		printf	msg
-		rts
+		printf	#msg
+		BRA     DONE
 
-msg:	'Vous avez pousse le boutton', CR, LF
+msg:	DC.B    'Vous avez poussé le boutton', $0D, $00
+
+
+;**************************************************************
+;*                          DEBUG                             *
+;**************************************************************
+
+                INCLUDE     'D_BUG12M.ASM'
 		
 ;**************************************************************
 ;*                 Interrupt Vectors                          *
