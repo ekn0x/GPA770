1c15f8482b77abffc8982f3e41ce3f02674b67df
Alex H.Lamarche <alex.hlamarche@gmail.com>
2017-11-09 10:22:31 -0500

    Update Lab2C.asm

--- LAb2/Lab2C.asm
+++ LAb2/Lab2C.asm
@@ -106,77 +106,7 @@ initTimer:
             
             MOVB    #$80,TSCR1          ; Activer le module TIM 
             RTS
-            
-            
-Calcul:		; Calculer DeltaV, TA, TC
-		PSHY				; Mettre sur la stack le registre Y
 
-	; Calcul de TA
-		LDX		#05			; Mettre 5 decimal dans le registre X
-		IDIV				; Diviser registre D par registre X
-		STX		TA			; Enregistre la valeur de TA dans la RAM
-
-	; Calcul de TC
-		LDD		TA			; Mettre dans le registre D, la valeur de TA
-		LDY 	#04			; Mettre 4 decimal dans le registre Y
-		EMUL				; Multiplier registre D par registre Y
-		STD		TC			; Calcul de TC
-
-	; Calcul de DeltaV
-		PULD				; Ramener la vitesse constante
-		SUBD	#Neutre		; Soustraire la valeur decimale 3000 au registre D
-		LDX		#10			;
-		IDIV				; Diviser Registre D par registre X
-		STX		DeltaV		; Enregistre la valeur de DeltaV dans la RAM
-
-	; Fin de calcul
-		rts
-
-Profil:		; ?crire les valeurs du profil de vitesse dans les tableaux
-		;	ramp down
-		MOVB	#10,COMPT 	; Assignation de 10 dans le compteur
-		LDD		#Neutre		; Charger la valeur neutre dans le registre D
-
-RampUD:
-		ADDD	X
-		STD		2,Y+		; Enregistrer le registre D et decaler l'addr du registre Y
-		DEC 	COMPT		; decrement compteur
-		BNE 	RampUD
-
-		MOVB	#10,COMPT 	; Assignation de 10 dans le compteur
-ConstD:
-		STD		2,Y+
-		DEC 	COMPT		; decrement compteur
-		BNE 	ConstD
-
-		MOVB	#10,COMPT 	; Assignation de 10 dans le compteur
-RampDD:
-		SUBD	X
-		STD		2,Y+
-		DEC 	COMPT		; decrement compteur
-		BNE 	RampDD
-
-		MOVB	#10,COMPT 	; Assignation de 10 dans le compteur
-RampDG:
-		SUBD	X
-		STD		2,Y+
-		DEC 	COMPT		; decrement compteur
-		BNE 	RampDG
-
-		MOVB	#10,COMPT 	; Assignation de 10 dans le compteur
-ConstG:
-		STD		2,Y+
-		DEC 	COMPT		; decrement compteur
-		BNE 	ConstG
-
-		MOVB	#10,COMPT 	; Assignation de 10 dans le compteur
-RampUG:
-		ADDD	X
-		STD		2,Y+
-		DEC 	COMPT		; decrement compteur
-		BNE 	RampUG
-
-		rts
 ;**************************************************************
 ;* Messages textes
 ;**************************************************************
@@ -184,6 +114,8 @@ RampUG:
 ;**************************************************************
 ;* Inclusion du fichier D_BUG12M.ASM
 ;**************************************************************
+			INCLUDE		'Lab2C_speedProfile.asm' ; Fonction de calcule de vitesse et de profile de vitesse
+			
 			INCLUDE 	'D_BUG12M.ASM' 		; Fichier pour la simulation des
                                             ; fonctions D_BUG12
  
